3a085f0a1033bab857adc9d1601133f67f26c0b9
Kassim Abdullah <kassim.abdullah@funneltree.net>
2020-01-26 16:11:40 +0800

    Updated models - added item and changed name of InspectionCategory

--- inspectv1/models.py
+++ inspectv1/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 # Create your models here.
-class InspectionCatgory(models.Model):
+class InspectionCategory(models.Model):
     category = models.CharField("Category", max_length=50)
     
 
@@ -15,3 +15,16 @@ class InspectionCatgory(models.Model):
     def get_absolute_url(self):
         return reverse("category_detail", kwargs={"pk": self.pk})
  
+class ItemInCategory(models.Model):
+    category = models.ForeignKey("InspectionCategory", verbose_name="Items", on_delete=models.CASCADE)
+    items = models.CharField("Item", max_length=20)
+
+    class Meta:
+        verbose_name = "Item"
+        verbose_name_plural = "Items"
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("item_detail", kwargs={"pk": self.pk})

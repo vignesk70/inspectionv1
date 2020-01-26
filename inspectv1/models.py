71fb6dfc67652c8f23f261524e2a4109d3c25fc1
Kassim Abdullah <kassim.abdullah@funneltree.net>
2020-01-26 15:54:19 +0800

    Model Created - InspectionCategory

--- inspectv1/models.py
+++ inspectv1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class InspectionCatgory(models.Model):
+    category = models.CharField("Category", max_length=50)
+    
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+
+    def __str__(self):
+        return self.category
+
+    def get_absolute_url(self):
+        return reverse("category_detail", kwargs={"pk": self.pk})
+ 

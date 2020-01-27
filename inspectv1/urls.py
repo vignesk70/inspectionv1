1471bb1ea20564c8058e21320df8aebd5772bae4
Kassim Abdullah <kassim.abdullah@funneltree.net>
2020-01-27 12:53:15 +0800

    Created Login pages

--- inspectv1/urls.py
+++ inspectv1/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path,include
 from . import views
+from django.contrib.auth import views as auth_views
+from django.contrib import admin
 
 app_name = 'inspectv1'
 urlpatterns = [
     #display home page
     path('',views.IndexView.as_view(), name='index'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]

548114aa6dd830506468b0727cd80b506b65a5db
Kassim Abdullah <kassim.abdullah@funneltree.net>
2020-01-27 15:25:36 +0800

    updated login pages

--- inspection/settings.py
+++ inspection/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inspectv1.apps.Inspectv1Config',
+    'bootstrap4',
 ]
 
 MIDDLEWARE = [
--- inspectv1/admin.py
+++ inspectv1/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from .models import *
-
+admin.AdminSite.site_header='Inspect'
 # Register your models here.
 admin.site.register(InspectionCategory)
 admin.site.register(ItemInCategory)

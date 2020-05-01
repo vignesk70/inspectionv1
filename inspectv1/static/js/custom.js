801bc81970424c450c9646d5edb3c8d0bc22253e
Kassim Abdullah <kassim.abdullah@funneltree.net>
2020-05-01 14:06:02 +0800

    geolocation search

--- inspectv1/static/js/custom.js
+++ inspectv1/static/js/custom.js
@@ -253,3 +253,22 @@ function showcard(id) {
     $("#category_" + id).addClass("visible");
   }
 }
+
+function geolocate() {
+  alert("GET LOCATION");
+  var x = document.getElementById("demo");
+
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+
+  function showPosition(position) {
+    x.innerHTML =
+      "Latitude: " +
+      position.coords.latitude +
+      "<br>Longitude: " +
+      position.coords.longitude;
+  }
+}

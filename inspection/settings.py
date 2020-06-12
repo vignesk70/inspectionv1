ba5d60e82a12e0ab0c1f59a8f8a0bd47a8166b1b
Kassim Abdullah <kassim.abdullah@funneltree.net>
2020-06-12 12:29:23 +0800

    update settings for login redirect url

--- inspection/settings.py
+++ inspection/settings.py
@@ -168,8 +168,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT =  os.path.join(BASE_DIR, 'static/')
 
-LOGIN_REDIRECT_URL = '/inspect'
-LOGIN_URL = '/inspect/accounts/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGIN_URL = '/accounts/login/'
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media/')
 MEDIA_URL= "/media/"
 UPLOAD_PATH = '/media/images/uploads/'

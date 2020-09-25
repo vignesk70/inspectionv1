df19defc01484889fba067ab1dd951c24bccc46d
Kassim Abdullah <kassim.abdullah@funneltree.net>
2020-09-25 13:54:29 +0800

    added new category

--- inspectv1/models.py
+++ inspectv1/models.py
@@ -28,7 +28,7 @@ class ItemInCategory(models.Model):
     FIELDTYPE = (('checkbox', 'CheckBox'),
                  ('text', 'Textfield'), ('number', 'NumberField'), ('date', 'DateField'))
     ERRORTYPE = [('NONE', 'None'), ('STATUTORY', 'Statutory'),
-                 ('SAFETY', 'Safety'), ('ENGINEERING', 'Engineering'), ('OPERATIONS', 'Operations'), ('POWER', 'Power')]
+                 ('SAFETY', 'Safety'), ('ENGINEERING', 'Engineering'), ('OPERATIONS', 'Operations'), ('POWER', 'Power'), ('CORRECTIVE', 'Corrective')]
 
     category = models.ForeignKey("InspectionCategory", verbose_name="Category",
                                  on_delete=models.CASCADE, related_name='items', default=3)

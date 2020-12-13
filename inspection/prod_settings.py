cb30c8f8bdfc3af756c3187104d71c1fa9475d87
Vigneswaren Krishnamoorthy <vignes@Vigs-MacBook-Pro.local>
2020-12-13 20:19:23 +0800

    for debug

--- inspection/prod_settings.py
+++ inspection/prod_settings.py
@@ -1,11 +1,11 @@
-DEBUG = False
+DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME':  'inspection',
         'USER': 'inspection',
         'PASSWORD': 'inspectionadmin',
-        'HOST': 'localhost',
+        'HOST': 'app.navidadtech.com',
         'PORT': '5432',
     }
 }

e1f0c9ca8f83ad912394345856bf736e7d14c491
Vigneswaren Krishnamoorthy <vignes_k@yahoo.com>
2021-01-10 01:28:51 +0800

    updated setting files. need mark settings.

--- inspection/prod_settings.py
+++ inspection/prod_settings.py
@@ -9,3 +9,9 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'vigneswaren.krishnamoorthy@gmail.com'
+EMAIL_HOST_PASSWORD = 'ujdxkyeogvfcdsad'

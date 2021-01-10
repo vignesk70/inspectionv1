fc09e2f24e9353e12816a3e1ab4816247ffde5c6
markambrose70 <jemambrose@yahoo.com>
2021-01-10 21:15:22 +0800

    Updated email settings

--- inspection/prod_settings.py
+++ inspection/prod_settings.py
@@ -10,8 +10,8 @@ DATABASES = {
     }
 }
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST = 'smtp.mail.yahoo.com'
 EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER = 'vigneswaren.krishnamoorthy@gmail.com'
-EMAIL_HOST_PASSWORD = 'ujdxkyeogvfcdsad'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'navidad.engineering@yahoo.com'
+EMAIL_HOST_PASSWORD = 'jggsxjwidzsllhsc'

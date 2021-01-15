4071727b64f09e4ba7e99c3d72b0badab6152ac3
markambrose70 <jemambrose@yahoo.com>
2021-01-15 17:13:51 +0800

    updated login redirect

--- inspection/settings.py
+++ inspection/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from django.core.urlresolvers import reverse_lazy
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -171,7 +172,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-LOGIN_REDIRECT_URL = 'login_success'
+LOGIN_REDIRECT_URL = reverse_lazy('login_success')
 LOGIN_URL = '/accounts/login/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = "/media/"

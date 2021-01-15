7206a912d654d30ad1d2d9dd229f9906336ab2c0
markambrose70 <jemambrose@yahoo.com>
2021-01-15 18:50:11 +0800

    updated login redirect

--- inspection/settings.py
+++ inspection/settings.py
@@ -172,7 +172,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
-LOGIN_REDIRECT_URL = reverse_lazy('login_success')
+LOGIN_REDIRECT_URL = 'inspectv1:login_success'
 LOGIN_URL = '/accounts/login/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 MEDIA_URL = "/media/"
